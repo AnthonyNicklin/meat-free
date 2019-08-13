7d5904118e6f941b64d85eeea478fa999b657cc4
Anthony <an@doetaylor.com>
2019-08-13 11:03:54 +0100

    Shadow to titles

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ import os
 class Config(object):
     """ Main configuration variables """
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MONGO_URI = os.environ.get('MONGO_URI')
-    MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'change-this'
+    MONGO_URI = 'mongodb+srv://antAdmin:dbfirstAdmin@mf-cluster-01-fnb1e.mongodb.net/mf-db-01?retryWrites=true&w=majority'
+    MONGO_DBNAME = 'mf-db-01'
